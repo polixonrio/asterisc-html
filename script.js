31b71b4126d8ea74f239e039fdc8ee6ba392aa2d
xilihulu <xilihulu@pm.me>
2023-01-14 09:57:22 +0530

    initial

--- script.js
+++ script.js
@@ -7,30 +7,91 @@ function setFormMessage(formElement, type, message) {
 }
 
 function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+    inputElement.classList.add("border", "border-danger");
+    inputElement.parentElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+    inputElement.parentElement.parentElement.parentElement.parentElement.querySelector(".card").classList.add("border", "border-danger");
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
+function clearInputError(inputElement) {
+    inputElement.classList.remove("border", "border-danger");
+    inputElement.parentElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+    inputElement.parentElement.parentElement.parentElement.parentElement.querySelector(".card").classList.remove("border", "border-danger");
 
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+}
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    console.log(createAccountForm);
+    document.querySelector("#linkLogin ").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.add("d-none");
         createAccountForm.classList.remove("d-none");
     });
 
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("d-none");
-        createAccountForm.classList.add("d-none");
-    });
+    // document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    //     e.preventDefault();
+    //     loginForm.classList.remove("d-none");
+    //     createAccountForm.classList.add("d-none");
+    // });
 
-    loginForm.addEventListener("submit", e=> {
-        e.preventDefault();
-        setFormMessage(loginForm, "error", "Invalid username/password combination")
-    });
+    // loginForm.addEventListener("submit", e=> {
+    //     e.preventDefault();
+    //     setFormMessage(loginForm, "error", "Invalid username/password combination")
+    // });
 
-    document.querySelectorAll(".form__input").forEach(inputElement)
+    
+
+
+    document.querySelectorAll(".form__input.form-control").forEach(inputElement => {
+        let emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let phonenoregex = /^\d{10}$/
+        
+        inputElement.addEventListener("blur", e => {
+            console.log(emailregex.test(e.target.value));
+            if (e.target.id === "ebox" && e.target.value=="" ) {
+                setInputError(inputElement, "This is a required question");
+            }
+            else if(e.target.id === "ebox" && !(emailregex.test(e.target.value))){
+                setInputError(inputElement, "Must be a valid email");
+                }
+            else if(e.target.id === "ebox"){
+                clearInputError(inputElement);
+            }
+        });
+
+
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "fname"  && e.target.value == "" ) {
+                setInputError(inputElement, "This is a required question");
+            }
+            else if(e.target.id === "fname"){
+                clearInputError(inputElement);
+            }
+        });
+
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "telnam" && e.target.value=="" ) {
+                setInputError(inputElement, "This is a required question");
+            }
+            else if(e.target.id === "telnam" && !(phonenoregex.test(e.target.value))){
+                setInputError(inputElement, "Must be a number");
+                }
+            else if(e.target.id === "telnam" && e.target.id === "telnam"){
+                clearInputError(inputElement);
+            }
+        });
+  
+    })
 })
